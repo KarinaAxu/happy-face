96e9990b80d862f57962e5e10f67fd62cc338f09
KarinaAxu <156989064+KarinaAxu@users.noreply.github.com>
2024-03-07 19:33:14 +0600

    Add files via upload
    Signed-off-by: KarinaAxu <156989064+KarinaAxu@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,121 @@
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const headerActive = function () {
+  if (window.scrollY > 150) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", headerActive);
+
+let lastScrolledPos = 0;
+
+const headerSticky = function () {
+  if (lastScrolledPos >= window.scrollY) {
+    header.classList.remove("header-hide");
+  } else {
+    header.classList.add("header-hide");
+  }
+
+  lastScrolledPos = window.scrollY;
+}
+
+addEventOnElem(window, "scroll", headerSticky);
+
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function () {
+  for (let i = 0; i < sections.length; i++) {
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 2) {
+      sections[i].classList.add("active");
+    }
+  }
+}
+
+scrollReveal();
+
+addEventOnElem(window, "scroll", scrollReveal);
+
+
+
+const modal = document.getElementById("myLorm");
+
+const openModalBtn = document.getElementById("openModalBtn");
+
+const closeBtn = document.getElementsByClassName("close")[0];
+
+openModalBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
+function SubForm (){
+  $.ajax({
+    url:"https://api.apispreadsheets.com/data/NMA1EtG9r4Puygf1/",
+    type:"post",
+    data:$("#myForm").serializeArray(),
+    success: function(){
+      alert("Form Data Submitted :)")
+    },
+    error: function(){
+      alert("There was an error :(")
+    }
+  });
+}
+function searchFunction() {
+  var input = document.getElementById('myInput');
+  var filter = input.value.toUpperCase();
+  var ul = document.getElementById("myList");
+  var h3 = ul.getElementsByTagName("h3");
+  for (var i = 0; i < h3.length; i++) {
+      var textValue = h3[i].innerText;
+      if (textValue.toUpperCase().indexOf(filter) > -1) {
+          h3[i].parentNode.parentNode.style.display = "";
+      } else {
+          h3[i].parentNode.parentNode.style.display = "none";
+      }
+  }
+}
